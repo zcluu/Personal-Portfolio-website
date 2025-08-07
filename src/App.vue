7d6a9476df2852ca606e928a44b888d2295041d9
DMSintalor <1256234567@qq.com>
2025-08-07 11:37:53 +0800

    feat: add dark btn

--- src/App.vue
+++ src/App.vue
@@ -1,71 +1,59 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import {RouterLink, RouterView} from 'vue-router'
+import {ref, watch, onMounted} from "vue";
+import {Sunny, Moon} from '@element-plus/icons-vue';
+import SwitchDarkButton from "@/components/SwitchDarkButton.vue";
+
+const isDark = ref(false);
+
+const switchBtnClassName = ref("switch-btn-dark");
+
+watch(isDark, () => {
+  switchBtnClassName.value = isDark.value ? 'switch-btn-white' : 'switch-btn-dark'
+
+  const html = document.documentElement
+  if (isDark.value) {
+    html.classList.add('dark')
+  } else {
+    html.classList.remove('dark')
+  }
+
+  localStorage.setItem('theme', isDark.value ? 'dark' : 'light')
+})
+
+onMounted(() => {
+  const theme = localStorage.getItem('theme')
+  if (theme === 'dark') {
+    isDark.value = true
+  }
+})
+
 </script>
 
 <template>
-  <RouterView />
+  <el-container class="dark">
+    <el-header style="display: flex;">
+      <div class="logo"></div>
+      <div style="flex: 1"></div>
+      <el-space :size="30">
+        <el-link
+            href="https://scholar.google.com/citations?user=byXIEVMAAAAJ&hl=en"
+            target="_blank"
+        >Google Scholar
+        </el-link>
+        <el-link
+            href="https://cslzc.py00.top/#/"
+            target="_blank"
+        >About Me
+        </el-link>
+        <el-link href="mailto:zclu@zju.edu.cn">Contact Me</el-link>
+        <SwitchDarkButton/>
+      </el-space>
+    </el-header>
+    <el-main></el-main>
+  </el-container>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
